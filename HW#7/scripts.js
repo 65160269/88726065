704b3f15b4e75174da1c076ecd5b0d1f071f22c2
Arnat Ueararee <65160269@go.buu.ac.th>
2024-01-17 19:16:52 +0700

    comment#5
    ‘toggle รายการ Todo’

--- HW#7/scripts.js
+++ HW#7/scripts.js
@@ -21,6 +21,11 @@ document.addEventListener("DOMContentLoaded", function () {
     todos.splice(index, 1);
     renderTodoList();
     }
+    // ตรวจสอบ/ยกเลิกการเสร็จสิ้นรายการ Todo
+    function toggleComplete(index) {
+    todos[index].completed = !todos[index].completed;
+    renderTodoList();
+    }
     function renderTodoList() {
     console.log(todos);
     todoList.innerHTML = "";
